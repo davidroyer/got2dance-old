999f24944cad34896024dd8d9101b27366b8b077
David Royer <droyer01@gmail.com>
2015-08-27 22:20:26 -0400

    Amimation for toggle is complete

--- js/main.js
+++ js/main.js
@@ -14,12 +14,12 @@
     // 	$('.nav-toggle').toggleClass('nav--open');
     // }
 
-    function openNavDrawer(event) {
-        event.preventDefault();	
-        navToggle.toggleClass('rotate--icon');
-        nav.toggleClass('nav--open');
+    // function openNavDrawer(event) {
+    //     event.preventDefault();	
+    //     navToggle.toggleClass('rotate--icon');
+    //     nav.toggleClass('nav--open');
 
-    }
+    // }
 
   //   function closeNavDrawer(event) {
   //       event.preventDefault();	
@@ -31,12 +31,46 @@
 
   //   }
 
+  	function whichTransitionEvent(){
+  	  var t,
+  	      el = document.createElement("fakeelement");
 
+  	  var transitions = {
+  	    "transition"      : "transitionend",
+  	    "OTransition"     : "oTransitionEnd",
+  	    "MozTransition"   : "transitionend",
+  	    "WebkitTransition": "webkitTransitionEnd"
+  	  };
 
-    navToggle.on('click', openNavDrawer);
+  	  for (t in transitions){
+  	    if (el.style[t] !== undefined){
+  	      return transitions[t];
+  	    }
+  	  }
+  	}
+
+  	var transitionEvent = whichTransitionEvent();
+  	
+
+    navToggle.on('click', function() {
+
+    	 event.preventDefault();
+
+    	 navToggle.toggleClass('rotate--icon').one(transitionEvent,
+
+	 		function(){
+	 			toggleMenu();
+	 		});
+    });
+
+    function toggleMenu() {
+    	nav.toggleClass('nav--open');
+    }
+
 
 })();
 
 
+// navToggle.on('click', openNavDrawer);
 
 
